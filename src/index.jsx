24832f7260a8c27ebd752559b7a9f1278e56862a
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-11 06:06:54 +0000

    added players page

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Players from "./pages/Players";
 
 const router = createBrowserRouter([
   {
-    path : "/",
-    element: <App/>
-  }
-])
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/players",
+    element: <Players />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-<RouterProvider router={router}/>
-	</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
